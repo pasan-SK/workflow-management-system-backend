0664ac215f0ccd54012a57c5c5a5e259bff6a6f2
KabilanMA <kabilanen@gmail.com>
2022-11-16 11:41:25 +0530

    Frontend deployment test1

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5501',
     'http://localhost:3500',
     'http://localhost:5500',
-	'http://localhost:3000'
+	'http://localhost:3000',
+    'https://63747b72c432170a063e454c--delightful-licorice-d3ecf6.netlify.app'
 	
 ];
 
